20879692f761bf4a07063196682e7f24d00e3deb
Jason Meyer <meyer066@cougars.csusm.edu>
2016-04-23 18:05:18 -0700

    working segue from buttons to respective views

---
+++ MapViewController.swift
@@ -0,0 +1,15 @@
+//
+//  MapViewController.swift
+//  simpletable
+//
+//  Created by Jason Meyer on 4/23/16.
+//  Copyright © 2016 Jason Meyer. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+class MapViewController: UIViewController {
+
+    @IBOutlet var mapView: MKMapView!
+}
